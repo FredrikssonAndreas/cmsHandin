72e5d7b44a78acbebb8d918d4c8d590c9d0b37b3
Andreas Fredriksson <afredriksson89@gmail.com>
2024-09-18 14:15:10 +0200

    JS script added with fade function

---
+++ cmsHandin/wwwroot/scripts/fadeScript.js
@@ -0,0 +1,24 @@
+﻿export function fadeInOnScroll() {
+    const fadeInElement = document.querySelector('.about-us img.fade');
+   
+
+    function isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    function onScroll() {
+        if (isElementInViewport(fadeInElement)) {
+            fadeInElement.classList.add('visible');
+        }
+    }
+
+    window.addEventListener('scroll', onScroll);
+    onScroll(); 
+}
+
