002302528433dd5349466fa211fdb02737b0c62a
Andreas Fredriksson <afredriksson89@gmail.com>
2024-10-01 09:17:06 +0200

    Changes to alot

---
+++ cmsHandin/Controllers/HomeSurfaceController.cs
@@ -0,0 +1,66 @@
+﻿using cmsHandin.Models;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Routing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Web.Website.Controllers;
+
+namespace cmsHandin.Controllers
+{
+    public class HomeSurfaceController : SurfaceController
+    {
+        public HomeSurfaceController(IUmbracoContextAccessor umbracoContextAccessor, IUmbracoDatabaseFactory databaseFactory, ServiceContext services, AppCaches appCaches, IProfilingLogger profilingLogger, IPublishedUrlProvider publishedUrlProvider) : base(umbracoContextAccessor, databaseFactory, services, appCaches, profilingLogger, publishedUrlProvider)
+        {
+        }
+
+        public IActionResult HandleSubmit(HomePageFormModel form)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewData["name"] = form.Name;
+                ViewData["email"] = form.Email;
+                ViewData["phone"] = form.Phone;
+
+                if (string.IsNullOrEmpty(form.Name))
+                {
+                    ViewData["error-name"] = "You must enter your name";
+                }
+                else if (ModelState.ContainsKey(nameof(form.Name)) && ModelState[nameof(form.Name)]?.Errors.Count > 0)
+                {
+                    ViewData["error-name"] = "Your name must contain at least two characters";
+                }
+
+
+                if (string.IsNullOrEmpty(form.Email))
+                {
+                    ViewData["error-email"] = "You must enter a valid email address";
+                }
+                else if (ModelState.ContainsKey(nameof(form.Email)) && ModelState[nameof(form.Email)]?.Errors.Count > 0)
+                {
+                    ViewData["error-email"] = "Invalid email address ex: ex@domain.com";
+                }
+
+                ViewData["error-phone"] = string.IsNullOrEmpty(form.Phone);
+
+                if (string.IsNullOrEmpty(form.Name))
+                {
+                    ViewData["error-phone"] = "You must enter a phone number";
+                }
+                else if (ModelState.ContainsKey(nameof(form.Phone)) && ModelState[nameof(form.Phone)]?.Errors.Count > 0)
+                {
+                    ViewData["error-phone"] = "Your phone number can only contain numbers";
+                }
+
+                return CurrentUmbracoPage();
+            }
+
+
+            TempData["submitted"] = "We have succesfully recived your info";
+            return RedirectToCurrentUmbracoPage();
+        }
+    }
+}
+
--- cmsHandin/wwwroot/scripts/validationScript.js
+++ cmsHandin/wwwroot/scripts/validationScript.js
@@ -1,22 +1,20 @@
-﻿export function validateForm() {
+export function validateForm() {
     const name = document.getElementById('name');
     const email = document.getElementById('email');
     const phone = document.getElementById('phone');
-    const emailPattern = /[A-Za-z0-9\._%+\-]+@[A-Za-z0-9\.\-]+\.[A-Za-z]{2,}/; 
+    const emailPattern = /[A-Za-z0-9._%+\-]+@[A-Za-z0-9.\-]+\.[A-Za-z]{2,}/;
+    const phonePattern = /^[0-9]{7,14}$/; 
 
-    
     name.addEventListener('input', function () {
         validateField(name, name.value.length >= 2);
     });
 
-   
     email.addEventListener('input', function () {
         validateField(email, emailPattern.test(email.value));
     });
 
-   
     phone.addEventListener('input', function () {
-        validateField(phone, phone.value.length >= 2);
+        validateField(phone, phonePattern.test(phone.value));
     });
 
     function validateField(field, isValid) {
